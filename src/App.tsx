8aeda227fbcb1b33d47530806f127a9760e30932
PropYouLike <propyoulike@gmail.com>
2025-12-02 13:23:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Index from "./pages/Index";
 import ProjectPage from "./pages/ProjectPage";
 import NotFound from "./pages/NotFound";
 import Tracking from "./templates/common/Tracking";
+
+// Optional placeholders (highly recommended)
+import BuilderPage from "./pages/BuilderPage";
+import LocalityPage from "./pages/LocalityPage";
 
 const queryClient = new QueryClient();
 
@@ -15,15 +20,24 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Tracking />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects/:slug" element={<ProjectPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      {/* Global tracking scripts */}
+      <Tracking />
+
+      {/* Your app routes */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+
+        {/* Dynamic project landing pages */}
+        <Route path="/projects/:slug" element={<ProjectPage />} />
+
+        {/* Optional pages required for widgets */}
+        <Route path="/builder/:builder" element={<BuilderPage />} />
+        <Route path="/locality/:locality" element={<LocalityPage />} />
+
+        {/* Catch all */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
